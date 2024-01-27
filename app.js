bd2359f45aa0b43341fe1d0277e8cad277ca10b1
Varun <VarunMK14@gmail.com>
2024-01-27 16:32:02 +0000

    Mobile display updated

--- app.js
+++ app.js
@@ -18,3 +18,12 @@ function getResponse() {
     document.getElementById("text_id").innerHTML = "ADVICE #" + res.slip.id + '\n';
     document.getElementById("text_advice").innerHTML = "\"" + res.slip.advice + "\"" + '\n';
   }
+
+  function getDivider() {
+    if (navigator.userAgentData.mobile) {
+        document.getElementById("divider-mobile").innerHTML = `<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`
+    }
+    else {
+        document.getElementById("divider-desktop").innerHTML = `<svg width="444" height="16" stxmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`
+    }
+  }
