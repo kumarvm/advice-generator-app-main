26731f1b5bade836bf5dc352a225b0d45930a6d1
Varun <VarunMK14@gmail.com>
2024-01-27 16:32:29 +0000

    Updated

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function getResponse() {
       if (httpRequest.status == 200) {
         const res = httpRequest.response;
         write(res);
+        divider();
       }
       else {
         console.log(`Error:" ${http.status}`);
@@ -19,11 +20,20 @@ function getResponse() {
     document.getElementById("text_advice").innerHTML = "\"" + res.slip.advice + "\"" + '\n';
   }
 
-  function getDivider() {
-    if (navigator.userAgentData.mobile) {
-        document.getElementById("divider-mobile").innerHTML = `<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`
+  function detectMob() {
+    return ( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) );
+  }
+
+  function divider() {
+    var el = document.getElementById('divider');
+    var content;
+
+    if (detectMob()) {
+        content = '<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>';
     }
     else {
-        document.getElementById("divider-desktop").innerHTML = `<svg width="444" height="16" stxmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>`
+        content = '<svg width="444" height="16" stxmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>';
     }
+
+    el.innerHTML = content;
   }
